24c3d2dfa1635ac6cc59dbe8d08b1daf7b48b909
tpserit <tpserit@gmail.com>
2023-04-17 15:22:44 +0200

    firebase

---
+++ script/firebase-messaging-sw.js
@@ -0,0 +1,23 @@
+﻿importScripts("https://www.gstatic.com/firebasejs/7.2.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/7.2.1/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/7.2.1/firebase-analytics.js");
+
+firebase.initializeApp({
+    messagingSenderId: "652360606753 "
+  });
+
+  const messaging = firebase.messaging();
+  
+  messaging.setBackgroundMessageHandler(payload => {
+    const notification = JSON.parse(payload.data.notification);
+    const notificationTitle = notification.title;
+    const notificationOptions = {
+      body: notification.body
+    };
+    //Show the notification :)
+    return self.registration.showNotification(
+      notificationTitle,
+      notificationOptions
+    );
+  });
+  
---
+++ script/index.js
@@ -0,0 +1,46 @@
+﻿const tokenString = document.getElementById("token");
+const errorMessage = document.getElementById("error");
+const message = document.getElementById("message");
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDuqHcMxy8_zSSTMWsjSJae-epC6sHYgTY",
+  
+    authDomain: "bktest-d2bb9.firebaseapp.com",
+  
+    projectId: "bktest-d2bb9",
+  
+    storageBucket: "bktest-d2bb9.appspot.com",
+  
+    messagingSenderId: "652360606753",
+  
+    appId: "1:652360606753:web:3eda13474ee78c9750ef60",
+  
+    measurementId: "G-VZX3F0LHJH"
+  
+  };
+firebase.initializeApp(config);
+const messaging = firebase.messaging();
+
+messaging
+  .requestPermission()
+  .then(() => {
+    message.innerHTML = "Notifications allowed";
+    return messaging.getToken();
+  })
+  .then(token => {
+    tokenString.innerHTML = "Token Is : " + token;
+  })
+  .catch(err => {
+    errorMessage.innerHTML = errorMessage.innerHTML + "; " + err;
+    console.log("No permission to send push", err);
+  });
+
+  messaging.onMessage(payload => {
+    console.log("Message received. ", payload);
+    const { title, ...options } = payload.notification;
+  });
+  
+
