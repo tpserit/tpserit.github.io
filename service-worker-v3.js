277d9d40ed9c816fb4eea3d6137c5103b240f2bd
tpserit <tpserit@gmail.com>
2023-04-17 11:51:30 +0200

    lots of things

---
+++ service-worker-v3.js
@@ -0,0 +1,10 @@
+﻿window.addEventListener("load", () => {
+    function handleNetworkChange(event) {
+      if (navigator.onLine) {
+        document.body.classList.remove("offline");
+      } else {
+        document.body.classList.add("offline");
+      }
+    }  window.addEventListener("online", handleNetworkChange);
+    window.addEventListener("offline", handleNetworkChange);
+  });
