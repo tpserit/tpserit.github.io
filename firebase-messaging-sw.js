b9714df959a614326b5d69057e9245bf29449350
tpserit <tpserit@gmail.com>
2023-04-18 15:00:24 +0200

    hhhh

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-﻿importScripts("https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/9.19.1/firebase-messaging.js");
+﻿importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-messaging.js');
 firebase.initializeApp({
     messagingSenderId: "652360606753"
   });
